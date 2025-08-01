87d9dd09b91c53c48936650fe9091fb21a966ecb
aditya <aditya20oktober@gmail.com>
2025-08-01 14:03:58 +0700

    get profile and logout

--- api-tick-track/app/Http/Controllers/AuthController.php
+++ api-tick-track/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
@@ -30,6 +31,41 @@ class AuthController extends Controller
         } catch (Exception $th) {
             return response()->json([
                 'message' => 'Login failed',
+                'error' => $th->getMessage(),
+                'data' => null
+            ], 500);
+        }
+    }
+
+    public function me(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            return response()->json([
+                'message' => 'Profile retrieved successfully',
+                'data' => new UserResource($user)
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Failed to retrieve user',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            $user->currentAccessToken()->delete();
+
+            return response()->json([
+                'message' => 'Logout successful',
+                'data' => null
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Logout failed',
                 'error' => $th->getMessage(),
                 'data' => null
             ], 500);
--- api-tick-track/routes/api.php
+++ api-tick-track/routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
